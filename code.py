b0b48888d1177fc0405e40ccd80566454b5bbef9
Michael Tjeng <121295154+michael-tjeng@users.noreply.github.com>
2023-10-28 11:44:26 +0800

    Read dataset using pandas
    Read dataset using pandas
    
    Used 'latin1' encoding to read dataset
    
    Set display options to show all columns when printing the head of the dataframe

--- code.py
+++ code.py
@@ -6,3 +6,13 @@ Created on Sat Oct 28 10:50:36 2023
 @author: michaeltjeng
 """
 
+import pandas as pd
+
+# Load the dataset
+data = pd.read_csv('data.csv', encoding='latin1')
+
+# Set display  options to show all columns
+pd.set_option('display.max.columns', None)
+
+# Show the first few rows of the dataset
+print(data.head())
