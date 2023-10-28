5a0abac72e7c5bfb2d170b0c085cb275640bb966
Michael Tjeng <121295154+michael-tjeng@users.noreply.github.com>
2023-10-28 22:19:58 +0800

    Functionalise first graph

--- code.py
+++ code.py
@@ -10,49 +10,48 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
 
-# Load the dataset
-data = pd.read_csv('data.csv', encoding='latin1')
+# define function to load csv and preprocess data
+def load_and_preprocess_data(file_path):
+    data = pd.read_csv(file_path, encoding='latin1')
+    data['InvoiceDate'] = pd.to_datetime(data['InvoiceDate'], errors='coerce')
+    data = data.dropna(subset=['InvoiceDate'])
+    data['TotalSales'] = data['Quantity'] * data['UnitPrice']
+    data.set_index('InvoiceDate', inplace=True)
+    return data
 
-# Set display options to show all columns
-pd.set_option('display.max.columns', None)
+def get_monthly_sales(data):
+    return data.groupby('Country').resample('M')['TotalSales'].sum().reset_index()
 
-# Convert 'InvoiceDate' to datetime format
-data['InvoiceDate'] = pd.to_datetime(data['InvoiceDate'], errors='coerce')
+def get_top_countries(monthly_sales, n=5):
+    return monthly_sales.groupby('Country')['TotalSales'].sum().nlargest(n).index
 
-# Drop rows where 'InvoiceDate' could not be converted to datetime
-data = data.dropna(subset=['InvoiceDate'])
+def plot_sales_over_time(top_countries_data, top_countries):
+    plt.figure(figsize=(14, 7))
+    for country in top_countries:
+        country_data = top_countries_data[top_countries_data['Country'] == country]
+        plt.plot(country_data['InvoiceDate'], country_data['TotalSales'], label=country)
+    plt.title('Monthly Sales Over Time by Country')
+    plt.xlabel('Date')
+    plt.ylabel('Total Sales (£)')
+    plt.xticks(rotation=45)
+    plt.gca().xaxis.set_major_locator(MaxNLocator(nbins=6))
+    plt.gca().yaxis.set_major_formatter(plt.FuncFormatter(lambda x, loc: "{:,}".format(int(x))))
+    plt.legend(title='Country')
+    plt.grid(True)
+    plt.tight_layout()
+    plt.show()
+    
+# main program
+if __name__ == "__main__":
+    pd.set_option('display.max.columns', None)
+    
+    data = load_and_preprocess_data('data.csv')
+    monthly_sales = get_monthly_sales(data)
+    top_countries = get_top_countries(monthly_sales)
+    top_countries_data = monthly_sales[monthly_sales['Country'].isin(top_countries)]
+    
+    plot_sales_over_time(top_countries_data, top_countries)
 
-# Calculate total sales
-data['TotalSales'] = data['Quantity'] * data['UnitPrice']
 
-# Set InvoiceDate as the index
-data.set_index('InvoiceDate', inplace=True)
 
-# Resample the data by month and sum the total sales for each country
-monthly_sales = data.groupby('Country').resample('M')['TotalSales'].sum().reset_index()
 
-# Find the top 5 countries by total sales
-top_countries = monthly_sales.groupby('Country')['TotalSales'].sum().nlargest(5).index
-
-# Filter the data to include only the top countries
-top_countries_data = monthly_sales[monthly_sales['Country'].isin(top_countries)]
-
-# Line Plot: Monthly Sales Over Time by Country
-plt.figure(figsize=(14, 7))
-
-# Loop through each country and plot the data
-for country in top_countries:
-    country_data = top_countries_data[top_countries_data['Country'] == country]
-    plt.plot(country_data['InvoiceDate'], country_data['TotalSales'], label=country)
-
-# Formatting the plot
-plt.title('Monthly Sales Over Time by Country')
-plt.xlabel('Date')
-plt.ylabel('Total Sales (£)')
-plt.xticks(rotation=45)
-plt.gca().xaxis.set_major_locator(MaxNLocator(nbins=6))
-plt.gca().yaxis.set_major_formatter(plt.FuncFormatter(lambda x, loc: "{:,}".format(int(x))))
-plt.legend(title='Country')
-plt.grid(True)
-plt.tight_layout()
-plt.show()
